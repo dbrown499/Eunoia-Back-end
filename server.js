d0ebd4ec57454bf5c609f8c8e686c4a5cb0728b5
Diamond <dbrown@pursuit.org>
2024-12-09 23:05:51 -0500

    Initial commit

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = require("./app.js");
 
 // CONFIGURATION
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 
 // LOG: Stripe Initialization (Optional)
-console.log("Stripe initialized with key:", process.env.STRIPE_SECRET_KEY.slice(0, 8) + "...");
+// console.log("Stripe initialized with key:", process.env.STRIPE_SECRET_KEY.slice(0, 8) + "...");
 
 // LISTEN
 app.listen(PORT, () => {
